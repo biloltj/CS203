0f3e71993ee6e4cd664516aa72fa80fddf8e15bd
biloltj <bilol2006tj@gmail.combilol2006tj@gmail.com>
2026-01-30 23:00:52 +0500

    Do Exercise 1.1

---
+++ chapter-01-tutorial-introduction/Getting_Started/char.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 20;
+    char name[]="Bilol";
+    char degree='2';
+    printf("Hello,I'm  %s and I'm %d years old\nMy degree is %c year Student CS department \n",name,age,degree);
+
+}
