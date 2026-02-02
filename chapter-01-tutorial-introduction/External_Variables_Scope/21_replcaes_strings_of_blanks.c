76301c13d790cbd5b0633b2dd0ae99e974ddf727
biloltj <bilol2006tj@gmail.combilol2006tj@gmail.com>
2026-02-02 11:30:09 +0500

    Add External Variables

---
+++ chapter-01-tutorial-introduction/External_Variables_Scope/21_replcaes_strings_of_blanks.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+#define TABSTOP 8
+
+int get_line(char line[], int lim);
+
+int main()
+{
+    printf("Enter text (press 'q' and Enter to quit):\n");
+    char line[MAXLINE];
+    int i, col, space_count;
+
+    while (get_line(line, MAXLINE) > 0) {
+
+        if (line[0] == 'q' && line[1] == '\n')
+            break;
+
+        col = 0;
+        space_count = 0;
+
+        for (i = 0; line[i] != '\0'; i++) {
+            if (line[i] == ' ') {
+                space_count++;
+                if ((col + space_count) % TABSTOP == 0) {
+                    putchar('\t');
+                    space_count = 0;
+                }
+            } else {
+                while (space_count > 0) {
+                    putchar(' ');
+                    space_count--;
+                }
+                putchar(line[i]);
+                if (line[i] == '\n')
+                    col = 0;
+                else
+                    col++;
+            }
+        }
+    }
+    return 0;
+}
+
+int get_line(char s[], int lim)
+{
+    int c, i;
+
+    for (i = 0; i < lim - 1 &&
+        (c = getchar()) != EOF && c != '\n'; i++)
+        s[i] = c;
+
+    if (c == '\n')
+        s[i++] = c;
+
+    s[i] = '\0';
+    return i;
+}
